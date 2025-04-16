c5252a6230a1dd4c00616a4457be12baab475f1c
Omlondhe307 <omlondhe307@gmail.com>
2025-04-16 08:40:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "12380656"))
+    API_HASH = environ.get("API_HASH", "d927c13beaaf5110f25c505b7c071273")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "BQC86fAAicn4-NVo83JtfSRmmtD_jk8EPGXwPMCTz1jXu5q8iJ4b0QzyoKkgpDN0v7Lr5OF5WTfe3ronm9yrZ2-jeEQ8z5EAxU-8dWXTny88ZTLPwgFGmvrituxwgJUPebeRUrG1_KTjy0qmKzRu_QUS5YnucEWdHp76E6Ihz5czTAbrmH6bILw9g1xI7lOYXa-wy_-fWWz2c6CT4C4Ku3XPbmM7hrZkvrF9c-PsLdCZqcP-R4FhtYs4KRA2qMfGjeBkSOXJrfGcDWUVgXNMytibWZshIdMrY27w2EPn4JmEvRZb20lZd0ZTgLAJaLdyG9kFkWmRr5zjO6z3kvJzevMxxC7xHAAAAAHDlxTRAA")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "7504342868:AAGgx4x74I69kev6InVjFy4x-0CzzmfNM2c"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
